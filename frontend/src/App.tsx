2b50f6e9f2f2c2582588af462ebdb3a22a72fc00
Conner Groth <connergroth@gmail.com>
2025-10-28 19:05:37 -0600

    chore: add frontend entry files with TODO placeholders and temporarily disable CI/CD

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+// TODO: Create main App component with routing
+// References:
+// - https://reactrouter.com/en/main/start/tutorial
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+
+// TODO: Import and add AuthProvider wrapper here
+// TODO: Create basic routing structure with Routes
+// TODO: Add placeholder routes for Login, Home, and 404 pages
+
+function App() {
+  return (
+    <div>
+      {/* TODO: Wrap with AuthProvider */}
+      {/* TODO: Add BrowserRouter with Routes */}
+      <h1>App Component - TODO: implement routing</h1>
+    </div>
+  );
+}
+
+export default App;
+
