5262c80e254d49356ec48a57015073999187605b
JT1350 <jskang1350@gmail.com>
2025-11-17 11:12:17 -0700

    feat: add reusable LoginButton component

--- frontend/src/components/ui/LoginButton.tsx
+++ frontend/src/components/ui/LoginButton.tsx
@@ -1,1 +1,20 @@
 // TODO: Create LoginButton component
+import { useState } from 'react';
+
+export default function LoginButton({ label }) {
+  const [is_button_loading, set_button_loading] = useState(false);
+  let button_text;
+
+  function handleClick() {
+    set_button_loading(true);
+    console.log('Login Clicked');
+  }
+
+  if (is_button_loading) {
+    button_text = 'Loading...';
+  } else {
+    button_text = label;
+  }
+
+  return <button className="bg-green-400 hover:bg-gray-400 focus:bg-red-400 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>{button_text}</button>;
+}
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,1 +1,6 @@
 // TODO: Create Login page
+import LoginButton from '../components/ui/LoginButton';
+
+export default function Login() {
+  return <LoginButton label={'Login'} />;
+}
